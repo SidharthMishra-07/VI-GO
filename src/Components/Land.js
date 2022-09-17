2d209c347d2e852d4f87d0c542e1ddc43bc79f74
Sidharth Mishra <sidharth1604@gmail.com>
2022-09-17 17:18:15 +0530

    Co-authored-by: BiswajeetRaut <BiswajeetRaut@users.noreply.github.com>

---
+++ src/Components/Land.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Land() {
+  return (
+    <div>
+      <div className="page">
+        <div className="top"></div>
+        <div className="bottom"></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Land
